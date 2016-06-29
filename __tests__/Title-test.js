b5be10e17e4f9cf991df01595990f32d8cd36504
Guillaume <grrseguin@gmail.com>
2016-06-29 21:42:16 +0200

    feat (jest) implementation

---
+++ __tests__/Title-test.js
@@ -0,0 +1,22 @@
+jest.unmock('../src/components/Title');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Title from '../src/components/Title';
+
+describe('Title', () => {
+
+  it('changes the text after click', () => {
+    // Render a checkbox with label in the document
+    const title = TestUtils.renderIntoDocument(
+      <Title>Hello, world! I am a title.</Title>
+    );
+
+    const titleNode = ReactDOM.findDOMNode(title);
+
+    // Verify that it's Off by default
+    expect(titleNode.textContent).toEqual('Hello, world! I am a title.');
+  });
+
+});
