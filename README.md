4f13643c7edf0082baed802b5d2587b2c75a1025
Guillaume <grrseguin@gmail.com>
2016-06-29 15:57:31 +0200

    Initaialization

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReactJS & webpack starter for Github issues
+
+## Get started
+```
+npm start
+```
---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Title = React.createClass({
+  render: function() {
+    return (
+      <h1 className="commentBox">
+        Hello, world! I am a title.
+      </h1>
+    );
+  }
+});
+
+export default Title;
